f0cb08c99a4d5be84805aae29f97865150e35c30
Nethmi-Vaas <anuvaas15@gmail.com>
2024-05-18 19:31:46 +0530

    Drop down list changes

--- lib/dropD.dart
+++ lib/dropD.dart
@@ -1,21 +1,23 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class DropDown extends StatefulWidget {
-  const DropDown({super.key});
+  const DropDown({Key? key}) : super(key: key);
 
   @override
-  // ignore: library_private_types_in_public_api
-  _DropDownExampleState createState() => _DropDownExampleState();
+  _DropDownState createState() => _DropDownState();
 }
 
-class _DropDownExampleState extends State<DropDown> {
-  String selectedValue1 = 'Option 1';
-  String selectedValue2 = 'Option A';
-  String selectedValue3 = 'Choice 1';
+class _DropDownState extends State<DropDown> {
+  String? selectedMonth;
+  String? selectedDistrict;
+  String? selectedArea;
+  String? selectedCropType;
 
-  List<String> options1 = ['Option 1', 'Option 2', 'Option 3', 'Option 4'];
-  List<String> options2 = ['Option A', 'Option B', 'Option C', 'Option D'];
-  List<String> options3 = ['Choice 1', 'Choice 2', 'Choice 3', 'Choice 4'];
+  bool showAreaDropdown = false;
+  bool showCropTypeDropdown = false;
+
+  List<String> fetchedData = [];
 
   @override
   Widget build(BuildContext context) {
@@ -42,108 +44,329 @@ class _DropDownExampleState extends State<DropDown> {
           ),
         ),
       ),
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/img_removal_6.png'),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: Column(
-          children: [
-            const SizedBox(height: 50),
-            DropdownButton<String>(
-              value: selectedValue1,
-              onChanged: (newValue) {
-                setState(() {
-                  selectedValue1 = newValue!;
-                });
-              },
-              items: options1.map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-            ),
-            const SizedBox(height: 50),
-            DropdownButton<String>(
-              value: selectedValue2,
-              onChanged: (newValue) {
-                setState(() {
-                  selectedValue2 = newValue!;
-                });
-              },
-              style: const TextStyle(color: Colors.green),
-              items: options2.map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-            ),
-            const SizedBox(height: 50),
-            DropdownButton<String>(
-              value: selectedValue3,
-              onChanged: (newValue) {
-                setState(() {
-                  selectedValue3 = newValue!;
-                });
-              },
-              items: options3.map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-            ),
-            const SizedBox(height: 70.0),
-            SizedBox(
-              height: 50.0, 
-              width: 500.0, 
-              child: ElevatedButton(
-                onPressed: () {
-                  //submit
-                },
-                style: ButtonStyle(
-                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
-                  ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const SizedBox(height: 50),
+          _buildMonthDropdown(),
+          const SizedBox(height: 20),
+          _buildDistrictDropdown(),
+          const SizedBox(height: 20),
+          if (showAreaDropdown) _buildAreaDropdown(),
+          const SizedBox(height: 20),
+          if (showCropTypeDropdown) _buildCropTypeDropdown(),
+          const SizedBox(height: 40),
+          ElevatedButton(
+            onPressed: () {
+              if (selectedMonth != null &&
+                  selectedDistrict != null &&
+                  selectedArea != null &&
+                  selectedCropType != null) {
+                fetchDataFromFirestore(selectedMonth!, selectedDistrict!,
+                    selectedArea!, selectedCropType!);
+              } else {
+                print('Please select all options');
+              }
+            },
+            style: ButtonStyle(
+              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8.0),
                 ),
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8.0),
-                    gradient: const LinearGradient(
-                      colors: [
-                        Color(0xFF16764B),
-                        Color(0xFFF8B23C)
-                      ], 
-                      begin: Alignment.centerLeft,
-                      end: Alignment.centerRight,
-                    ),
-                  ),
-                  child: const Center(
-                    child: Padding(
-                      padding:
-                          EdgeInsets.symmetric(vertical: 5.0, horizontal: 5.0),
-                      child: Text(
-                        'Submit',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 16.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
+              ),
+            ),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8.0),
+                gradient: const LinearGradient(
+                  colors: [Color(0xFF16764B), Color(0xFFF8B23C)],
+                  begin: Alignment.centerLeft,
+                  end: Alignment.centerRight,
+                ),
+              ),
+              child: const Center(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+                  child: Text(
+                    'Submit',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
                 ),
               ),
-            )
-          ],
-        ),
+            ),
+          ),
+          const SizedBox(height: 20),
+          if (fetchedData.isNotEmpty) _buildFetchedDataList(),
+        ],
       ),
     );
+  }
+
+  Widget _buildMonthDropdown() {
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance.collection("Month").snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Center(
+            child: Text("Some error occurred ${snapshot.error}"),
+          );
+        }
+        List<DropdownMenuItem<String>> monthItems = [];
+        if (!snapshot.hasData) {
+          return const CircularProgressIndicator();
+        } else {
+          final selectMonth = snapshot.data?.docs;
+          if (selectMonth != null) {
+            for (var month in selectMonth) {
+              monthItems.add(
+                DropdownMenuItem(
+                  value: month.id,
+                  child: Text(
+                    month['name'],
+                  ),
+                ),
+              );
+            }
+          }
+          return Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey, width: 1),
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: DropdownButton<String>(
+                underline: const SizedBox(),
+                isExpanded: true,
+                hint: const Text(
+                  "Select the Month",
+                  style: TextStyle(fontSize: 20),
+                ),
+                value: selectedMonth,
+                items: monthItems,
+                onChanged: (value) {
+                  setState(() {
+                    selectedMonth = value;
+                  });
+                },
+              ),
+            ),
+          );
+        }
+      },
+    );
+  }
+
+  Widget _buildDistrictDropdown() {
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance.collection("District").snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Center(
+            child: Text("Some error occurred ${snapshot.error}"),
+          );
+        }
+        List<DropdownMenuItem<String>> districtItems = [];
+        if (!snapshot.hasData) {
+          return const CircularProgressIndicator();
+        } else {
+          final selectDistrict = snapshot.data?.docs;
+          if (selectDistrict != null) {
+            for (var district in selectDistrict) {
+              districtItems.add(
+                DropdownMenuItem(
+                  value: district.id,
+                  child: Text(
+                    district['D_name'],
+                  ),
+                ),
+              );
+            }
+          }
+          return Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey, width: 1),
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: DropdownButton<String>(
+                underline: const SizedBox(),
+                isExpanded: true,
+                hint: const Text(
+                  "Select the District",
+                  style: TextStyle(fontSize: 20),
+                ),
+                value: selectedDistrict,
+                items: districtItems,
+                onChanged: (value) {
+                  setState(() {
+                    selectedDistrict = value;
+                    showAreaDropdown = true;
+                  });
+                },
+              ),
+            ),
+          );
+        }
+      },
+    );
+  }
+
+  Widget _buildAreaDropdown() {
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance.collection("Area").snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Center(
+            child: Text("Some error occurred ${snapshot.error}"),
+          );
+        }
+        List<DropdownMenuItem<String>> areaItems = [];
+        if (!snapshot.hasData) {
+          return const CircularProgressIndicator();
+        } else {
+          final selectArea = snapshot.data?.docs;
+          if (selectArea != null) {
+            for (var area in selectArea) {
+              areaItems.add(
+                DropdownMenuItem(
+                  value: area.id,
+                  child: Text(
+                    area['A_name'],
+                  ),
+                ),
+              );
+            }
+          }
+          return Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey, width: 1),
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: DropdownButton<String>(
+                underline: const SizedBox(),
+                isExpanded: true,
+                hint: const Text(
+                  "Select the Area",
+                  style: TextStyle(fontSize: 20),
+                ),
+                value: selectedArea,
+                items: areaItems,
+                onChanged: (value) {
+                  setState(() {
+                    selectedArea = value;
+                    showCropTypeDropdown = true;
+                  });
+                },
+              ),
+            ),
+          );
+        }
+      },
+    );
+  }
+
+  Widget _buildCropTypeDropdown() {
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance.collection("Crop_Type").snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Center(
+            child: Text("Some error occurred ${snapshot.error}"),
+          );
+        }
+        List<DropdownMenuItem<String>> cropTypeItems = [];
+        if (!snapshot.hasData) {
+          return const CircularProgressIndicator();
+        } else {
+          final selectCropTypes = snapshot.data?.docs;
+          if (selectCropTypes != null) {
+            for (var cropType in selectCropTypes) {
+              cropTypeItems.add(
+                DropdownMenuItem(
+                  value: cropType.id,
+                  child: Text(
+                    cropType['CT_name'],
+                  ),
+                ),
+              );
+            }
+          }
+          return Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey, width: 1),
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: DropdownButton<String>(
+                underline: const SizedBox(),
+                isExpanded: true,
+                hint: const Text(
+                  "Select the Crop Type",
+                  style: TextStyle(fontSize: 20),
+                ),
+                value: selectedCropType,
+                items: cropTypeItems,
+                onChanged: (value) {
+                  setState(() {
+                    selectedCropType = value;
+                  });
+                },
+              ),
+            ),
+          );
+        }
+      },
+    );
+  }
+
+  Widget _buildFetchedDataList() {
+    return Expanded(
+      child: ListView.builder(
+        itemCount: fetchedData.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(fetchedData[index]),
+          );
+        },
+      ),
+    );
+  }
+
+  void fetchDataFromFirestore(String month, String district, String area, String cropType) {
+    FirebaseFirestore.instance
+        .collection("Month")
+        .doc(month)
+        .collection("District")
+        .doc(district)
+        .collection("Area")
+        .doc(area)
+        .collection("CropType")
+        .get()
+        .then((QuerySnapshot querySnapshot) {
+      setState(() {
+        fetchedData.clear();
+        for (var doc in querySnapshot.docs) {
+          fetchedData.add(doc['C1']); // C1 - field name
+          fetchedData.add(doc['C2']); 
+          fetchedData.add(doc['C3']); 
+          fetchedData.add(doc['C4']); 
+          fetchedData.add(doc['C5']); 
+          
+        }
+      });
+    });
   }
 }
